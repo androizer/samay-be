737927b7606654bfb35299a96b6ad7ac2571d520
chenchunaidu-hotstar <maddinenichenchunaidu@gmail.com>
2026-01-25 19:09:26 +0530

    feat: added code

--- src/modules/workspaces/types.ts
+++ src/modules/workspaces/types.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 import {
   CREATE_WORKSPACE_SCHEMA,
   UPDATE_WORKSPACE_SCHEMA,
-  DELETE_USER_QUERY_SCHEMA,
+  DELETE_USER_PARAM_SCHEMA,
   CREATE_INVITATION_SCHEMA,
   ACCEPT_INVITATION_SCHEMA,
   INVITATION_QUERY_SCHEMA,
@@ -10,7 +10,7 @@ import {
 
 export type CreateWorkspaceInput = z.infer<typeof CREATE_WORKSPACE_SCHEMA>;
 export type UpdateWorkspaceInput = z.infer<typeof UPDATE_WORKSPACE_SCHEMA>;
-export type DeleteUserInput = z.infer<typeof DELETE_USER_QUERY_SCHEMA>;
+export type DeleteUserInput = z.infer<typeof DELETE_USER_PARAM_SCHEMA>;
 export type CreateInvitationInput = z.infer<typeof CREATE_INVITATION_SCHEMA>;
 export type AcceptInvitationInput = z.infer<typeof ACCEPT_INVITATION_SCHEMA>;
 export type InvitationQuery = z.infer<typeof INVITATION_QUERY_SCHEMA>;
